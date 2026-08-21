f8fff2cc81ff7a7b0a06bf607599bafa83512019
laibazaheer1019 <laibazaheer1019@gmail.cm>
2026-08-21 23:30:56 +0500

    Library Management System

--- library/admin.py
+++ library/admin.py
@@ -1,18 +1,11 @@
 from django.contrib import admin
-from library.models import Author, Catagory,Publisher,Book
-
-class  AuthorAdmin(admin.ModelAdmin):
-    list_display = ('id','name','email','created_at','updated_at')
-    search_fields = ('name',)
-    ordering = ('id',)
-admin.site.register(Author, AuthorAdmin)
+from library.models import Catagory, Publisher, Book,Author,Authorbook,Booksdetail
 
 class  CatagoryAdmin(admin.ModelAdmin):
     list_display = ('id','name','created_at','updated_at')
     search_fields = ('name',)
     ordering = ('id',)
 admin.site.register(Catagory, CatagoryAdmin)
-
 
 class PublisherAdmin(admin.ModelAdmin):
     list_display = ('id','name','created_at','updated_at')
@@ -22,8 +15,30 @@ admin.site.register(Publisher, PublisherAdmin)
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('id','title','category','publisher','created_at','updated_at')
-    search_fields = ('title','category','publisher')
+    search_fields = ('title',)
     ordering = ('id',)
 admin.site.register(Book, BookAdmin)
+
+class  AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','created_at','updated_at')
+    search_fields = ('id','name',)
+    ordering = ('id',)
+admin.site.register(Author,AuthorAdmin)
+
+class  AuthorbookAdmin(admin.ModelAdmin):
+    list_display = ('id','author','book','created_at','updated_at')
+    search_fields = ('id',)
+    ordering = ('id',)
+admin.site.register(Authorbook,AuthorbookAdmin)
+
+class BooksdetailAdmin(admin.ModelAdmin):
+    list_display = ('id','isbn','pages','language','created_at','updated_at')
+    search_fields = ('isbn',)
+    ordering = ('id',)
+admin.site.register(Booksdetail, BooksdetailAdmin)
+
+
+
+
 
 
--- library/models.py
+++ library/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-class Author(models.Model):
-      name = models.CharField(max_length=255)
-      email = models.EmailField(max_length=255)
-      created_at = models.DateTimeField(auto_now_add=True)
-      updated_at = models.DateTimeField(auto_now=True)
-      def __str__(self):
-           return f'{self.name}'
-
 class Catagory(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -33,5 +25,39 @@ class Book(models.Model):
 
     def __str__(self):
         return f'{self.title} of {self.category} by {self.publisher}'
+
+class Author(models.Model):
+      name = models.CharField(max_length=255)
+      email = models.EmailField(max_length=255)
+      books = models.ManyToManyField(
+          Book,
+          through='Authorbook'
+      )
+
+      created_at = models.DateTimeField(auto_now_add=True)
+      updated_at = models.DateTimeField(auto_now=True)
+
+      def __str__(self):
+        return f'{self.name}'
+
+class Authorbook(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+          return f'{self.book.title} by {self.author.name}'
+
+class Booksdetail(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    isbn= models.CharField(max_length=255)
+    pages = models.IntegerField()
+    language = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.book.title} with {self.pages} in {self.language}'
 
 
